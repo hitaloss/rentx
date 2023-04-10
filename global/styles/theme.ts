c24c20b88d5c4fdab458970cb55ad4f0cbdfd04a
hitalo <hitalo.2014.hs@gmail.com>
2023-04-10 10:08:29 -0300

    style: added global style pallet

---
+++ global/styles/theme.ts
@@ -0,0 +1,32 @@
+export default {
+    colors: {
+        header: "#1B1B1F",
+        background: {
+            1: "#F4F5F6",
+            2: "#FFFFFF",
+        },
+        text: "#7A7A80",
+        caption: "#AEAEB3",
+        title: "#43434D",
+        line: "#EBEBF0",
+
+        main: {
+            main: "#DC3637",
+            light: "#FDEDEF",
+        },
+        success: "#03B252",
+        shape: {
+            main: "#E1E1E8",
+            dark: "#29292E"
+        },
+        
+
+    },
+    fonts: {
+        inter_regular: "Inter_400Regular",
+        inter_medium: "Inter_500Medium",
+        archivo_regular: "Archivo_400Regular",
+        archivo_medium: "Archivo_500Medium",
+        archivo_semibold: "Archivo_600SemiBold",
+    }
+}
