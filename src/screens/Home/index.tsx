f4727f4a6bbc38d3071dde717cb462a17778a26c
hitalo <hitalo.2014.hs@gmail.com>
2023-04-11 16:17:26 -0300

    feat: created mocked header component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container, Title } from "./styles";
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
+
+import Logo from "./../../assets/Logo.svg";
 
 function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
