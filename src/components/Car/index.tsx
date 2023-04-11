ba63e824aa1ad379f6de9d474bcb776a8bb926bc
hitalo <hitalo.2014.hs@gmail.com>
2023-04-11 19:29:45 -0300

    feat: created base component for Car card

---
+++ src/components/Car/index.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useTheme } from "styled-components/native";
+
+import {
+  Container,
+  Details,
+  Brand,
+  Name,
+  About,
+  Rent,
+  Period,
+  Price,
+  Type,
+  CarImage,
+} from "./styles";
+
+import GasSvg from "./../../assets/Drop.svg";
+
+interface CarData {
+  brand: string;
+  name: string;
+  rent: {
+    period: string;
+    price: number;
+  };
+  thumbnail: string;
+}
+
+interface Props {
+  data: CarData;
+}
+
+function Car({ data }: Props) {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <Details>
+        <Brand>{data.brand}</Brand>
+        <Name>{data.name}</Name>
+        <About>
+          <Rent>
+            <Period>{data.rent.period}</Period>
+            <Price>{`R$ ${data.rent.price}`}</Price>
+          </Rent>
+          <Type>
+            <GasSvg />
+          </Type>
+        </About>
+      </Details>
+
+      <CarImage
+        source={{
+          uri: data.thumbnail,
+        }}
+        resizeMode="contain"
+      />
+    </Container>
+  );
+}
+
+// "https://d2ivfcfbdvj3sm.cloudfront.net/7fc965ab77efe6e0fa62e4ca1ea7673bb25e4853091e3d8e88cb/stills_0640_png/MY2021/14922/14922_st0640_116.png"
+
+export default Car;
---
+++ src/components/Car/styles.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components/native';
+import { RFValue } from "react-native-responsive-fontsize"
+
+export const Container = styled.View`
+  width: 100%;
+  height: ${RFValue(126)}px;
+  background-color: ${({theme}) => theme.colors.background[1]};
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px;
+  margin-bottom: 16px;
+`;
+
+export const Details = styled.View`
+  
+`;
+
+export const Brand = styled.Text`
+  font-family: ${({theme}) => theme.fonts.archivo_medium};
+  color: ${({theme}) => theme.colors.caption};
+  font-size: ${RFValue(10)}px;
+  text-transform: uppercase;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({theme}) => theme.fonts.archivo_medium};
+  color: ${({theme}) => theme.colors.title};
+  font-size: ${RFValue(15)}px;
+  text-transform: capitalize;
+`;
+
+export const About = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 16px;
+`;
+
+export const Rent = styled.View`
+  margin-right: 24px;
+`;
+
+export const Period = styled.Text`
+  color: ${({theme}) => theme.colors.caption};
+  font-size: ${RFValue(10)}px;
+  font-family: ${({theme}) => theme.fonts.archivo_medium};
+  text-transform: uppercase;
+`;
+
+export const Price = styled.Text`
+  color: ${({theme}) => theme.colors.main.main};
+  font-size: ${RFValue(15)}px;
+  font-family: ${({theme}) => theme.fonts.archivo_medium};
+`;
+
+export const Type = styled.View`
+  color: ${({theme}) => theme.colors.caption};
+`;
+
+export const CarImage = styled.Image`
+  width: 168px;
+  height: 85px;
+`;
+
