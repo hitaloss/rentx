8b84ab48259e943bae67aaa3b3e225cf0607623d
hitalo <hitalo.2014.hs@gmail.com>
2023-04-11 16:21:19 -0300

    style: created styles for header component

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,13 +1,31 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-
   background-color: ${({ theme }) => theme.colors.shape.main};
 `;
 
 export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.inter_medium};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(113)}px;
+  background-color: ${({ theme }) => theme.colors.header};
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.inter_regular};
+  color: ${({ theme }) => theme.colors.text};
 `;
