bdd20641d636a2332f47b49ff5d5ad0c6bb2ea66
hitalo <hitalo.2014.hs@gmail.com>
2023-04-26 17:38:10 -0300

    feat: implemented date selection on calendar component feature

--- src/screens/Schedule/index.tsx
+++ src/screens/Schedule/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert, StatusBar } from "react-native";
 
 import {
   Container,
@@ -11,28 +12,99 @@ import {
   Content,
   Footer,
 } from "./styles";
-import BackButton from "../../components/BackButton";
 import { useTheme } from "styled-components";
+import BackButton from "../../components/BackButton";
+import Button from "../../components/Button";
+import {
+  Calendar,
+  MarkedDateProps,
+  DateObject,
+} from "../../components/Calendar";
+
+import { format } from "date-fns";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useRoute } from "@react-navigation/native";
+
+import createInterval from "../../components/Calendar/createInterval";
+import { getPlatformDate } from "../../utils";
 
 import ArrowSvg from "./../../assets/Thin_arrow_left.svg";
-import { StatusBar } from "react-native";
-import Button from "../../components/Button";
-import Calendar from "../../components/Calendar";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { ICar } from "../../interfaces/cars";
+
+interface Params {
+  car: ICar;
+}
 
 type RootStackParamList = {
-  ScheduleDetails: undefined;
+  ScheduleDetails: {
+    car: ICar;
+    dates: string[];
+  };
 };
 
 interface Props {
   navigation: NativeStackNavigationProp<RootStackParamList, "ScheduleDetails">;
 }
 
+interface RentalPeriodProps {
+  startFormatted: string;
+  endFormatted: string;
+}
+
 function Schedule({ navigation }: Props) {
+  const [lastSelectedDate, setLastSelectedDate] = useState<DateObject>(
+    {} as DateObject
+  );
+  const [markedDates, setMarkedDates] = useState<MarkedDateProps>(
+    {} as MarkedDateProps
+  );
+  const [rentalPeriod, setRentalPeriod] = useState<RentalPeriodProps>(
+    {} as RentalPeriodProps
+  );
+
   const theme = useTheme();
+  const route = useRoute();
+
+  const { car } = route.params as Params;
 
   const handleScheduleDetails = () => {
-    navigation.navigate("ScheduleDetails");
+    if (!rentalPeriod.startFormatted || !rentalPeriod.endFormatted) {
+      Alert.alert(
+        "Intervalo necessário",
+        "Selecione um intervalo para alugar."
+      );
+    } else {
+      navigation.navigate("ScheduleDetails", {
+        car,
+        dates: Object.keys(markedDates),
+      });
+    }
+  };
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
+  const handleChangeDates = (date: DateObject) => {
+    let start = !lastSelectedDate.timestamp ? date : lastSelectedDate;
+    let end = date;
+    if (start.timestamp > end.timestamp) {
+      start = end;
+      end = start;
+    }
+    setLastSelectedDate(end);
+    const interval = createInterval(start, end);
+    setMarkedDates(interval);
+    const firstDate = Object.keys(interval)[0];
+    const lastDate = Object.keys(interval)[Object.keys(interval).length - 1];
+
+    setRentalPeriod({
+      startFormatted: format(
+        getPlatformDate(new Date(firstDate)),
+        "dd/MM/yyyy"
+      ),
+      endFormatted: format(getPlatformDate(new Date(lastDate)), "dd/MM/yyyy"),
+    });
   };
 
   return (
@@ -43,7 +115,7 @@ function Schedule({ navigation }: Props) {
           translucent
           backgroundColor="transparent"
         />
-        <BackButton onPress={() => {}} color={theme.colors.shape.main} />
+        <BackButton onPress={handleGoBack} color={theme.colors.shape.main} />
         <Title>
           Escolha uma {"\n"}
           data de início e{"\n"}
@@ -53,20 +125,24 @@ function Schedule({ navigation }: Props) {
         <RentalPeriod>
           <DateInfo>
             <DateTitle>DE</DateTitle>
-            <DateValue selected={false}>18/06/2021</DateValue>
+            <DateValue selected={!!rentalPeriod.startFormatted}>
+              {rentalPeriod.startFormatted}
+            </DateValue>
           </DateInfo>
 
           <ArrowSvg />
 
           <DateInfo>
             <DateTitle>ATÉ</DateTitle>
-            <DateValue selected={false}>18/06/2021</DateValue>
+            <DateValue selected={!!rentalPeriod.endFormatted}>
+              {rentalPeriod.endFormatted}
+            </DateValue>
           </DateInfo>
         </RentalPeriod>
       </Header>
 
       <Content>
-        <Calendar />
+        <Calendar markedDates={markedDates} onDayPress={handleChangeDates} />
       </Content>
 
       <Footer>
