947b9598ec11ac49a80b601ecbb8f50c2f3f6ef9
hitalo <hitalo.2014.hs@gmail.com>
2023-04-27 18:47:30 -0300

    feat: added floating button for route redirection to UsersCars

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,31 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "react-native";
-import { RFValue } from "react-native-responsive-fontsize";
-import api from "../../services/api";
 
-import { CarList, Container, Header, HeaderContent, TotalCars } from "./styles";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Ionicons } from "@expo/vector-icons";
+import api from "../../services/api";
+import { useTheme } from "styled-components";
+
+import {
+  CarList,
+  Container,
+  Header,
+  HeaderContent,
+  TotalCars,
+  UserCarsButton,
+} from "./styles";
+import Car from "../../components/Car";
+import Loading from "../../components/Loading";
 
 import Logo from "./../../assets/Logo.svg";
-import Car from "../../components/Car";
 
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { ICar } from "../../interfaces/cars";
-import Loading from "../../components/Loading";
 
 type RootStackParamList = {
   CarDetails: { car: ICar };
+  UserCars: undefined;
 };
 
 interface Props {
-  navigation: NativeStackNavigationProp<RootStackParamList, "CarDetails">;
+  navigation: NativeStackNavigationProp<
+    RootStackParamList,
+    "CarDetails",
+    "UserCars"
+  >;
 }
 
 function Home({ navigation }: Props) {
   const [cars, setCars] = useState<ICar[]>([]);
   const [loading, setLoading] = useState(true);
 
+  const theme = useTheme();
+
   const handleCarDetails = (car: ICar) => {
     navigation.navigate("CarDetails", { car });
+  };
+
+  const handleUserCars = () => {
+    navigation.navigate("UserCars");
   };
 
   useEffect(() => {
@@ -66,6 +87,13 @@ function Home({ navigation }: Props) {
           keyExtractor={(item) => item.id}
         />
       )}
+      <UserCarsButton onPress={handleUserCars}>
+        <Ionicons
+          name="ios-car-sport"
+          size={32}
+          color={theme.colors.shape.main}
+        />
+      </UserCarsButton>
     </Container>
   );
 }
