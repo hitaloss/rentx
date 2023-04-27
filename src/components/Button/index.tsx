a67b6848bbd8eeb17315971c17d1f4b0d32b78e2
hitalo <hitalo.2014.hs@gmail.com>
2023-04-27 18:45:25 -0300

    feat: added some features on basic button components, such disabled mode and loading state

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ActivityIndicator } from "react-native";
 
 import { Container, Title } from "./styles";
 import { useTheme } from "styled-components";
@@ -8,16 +9,36 @@ interface Props extends RectButtonProps {
   title: string;
   color?: string;
   textColor?: string;
+  enabled?: boolean;
+  loading?: boolean;
 }
 
-function Button({ title, color, textColor, ...rest }: Props) {
+function Button({
+  title,
+  color,
+  textColor,
+  enabled = true,
+  loading = false,
+  ...rest
+}: Props) {
   const theme = useTheme();
 
   return (
-    <Container {...rest} color={color ? color : theme.colors.main.main}>
-      <Title textColor={textColor ? textColor : theme.colors.shape.main}>
-        {title}
-      </Title>
+    <Container
+      {...rest}
+      color={color ? color : theme.colors.main.main}
+      style={{
+        opacity: enabled === false || loading ? 0.5 : 1,
+      }}
+      enabled={enabled}
+    >
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape.main} />
+      ) : (
+        <Title textColor={textColor ? textColor : theme.colors.shape.main}>
+          {title}
+        </Title>
+      )}
     </Container>
   );
 }
