da82982d740ffc10b3f20c7b20f2329fd3deedf3
hitalo <hitalo.2014.hs@gmail.com>
2023-05-05 10:20:49 -0300

    feat: added movable button instead of the fixed button on home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, StyleSheet } from "react-native";
 
 import { RFValue } from "react-native-responsive-fontsize";
 import { Ionicons } from "@expo/vector-icons";
 import api from "../../services/api";
 import { useTheme } from "styled-components";
+import { RectButton, PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring,
+} from "react-native-reanimated";
+const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
 
-import {
-  CarList,
-  Container,
-  Header,
-  HeaderContent,
-  TotalCars,
-  UserCarsButton,
-} from "./styles";
+import { CarList, Container, Header, HeaderContent, TotalCars } from "./styles";
 import Car from "../../components/Car";
 import Loading from "../../components/Loading";
 
@@ -38,6 +39,30 @@ interface Props {
 function Home({ navigation }: Props) {
   const [cars, setCars] = useState<ICar[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const posY = useSharedValue(0);
+  const posX = useSharedValue(0);
+
+  const userCarsAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: posX.value }, { translateY: posY.value }],
+    };
+  });
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, ctx: any) {
+      ctx.posX = posX.value;
+      ctx.posY = posY.value;
+    },
+    onActive(event, ctx: any) {
+      posX.value = ctx.posX + event.translationX;
+      posY.value = ctx.posY + event.translationY;
+    },
+    onEnd() {
+      posX.value = withSpring(0);
+      posY.value = withSpring(0);
+    },
+  });
 
   const theme = useTheme();
 
@@ -73,7 +98,9 @@ function Home({ navigation }: Props) {
       <Header>
         <HeaderContent>
           <Logo width={RFValue(108)} height={RFValue(12)} />
-          <TotalCars>Total de 12 carros</TotalCars>
+          <TotalCars>
+            Total de {cars.length} {cars.length > 1 ? "carros" : "carro"}
+          </TotalCars>
         </HeaderContent>
       </Header>
       {loading ? (
@@ -87,15 +114,38 @@ function Home({ navigation }: Props) {
           keyExtractor={(item) => item.id}
         />
       )}
-      <UserCarsButton onPress={handleUserCars}>
-        <Ionicons
-          name="ios-car-sport"
-          size={32}
-          color={theme.colors.shape.main}
-        />
-      </UserCarsButton>
+
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[
+            userCarsAnimatedStyle,
+            { position: "absolute", bottom: 13, right: 22 },
+          ]}
+        >
+          <ButtonAnimated
+            style={[styles.button, { backgroundColor: theme.colors.main.main }]}
+            onPress={handleUserCars}
+          >
+            <Ionicons
+              name="ios-car-sport"
+              size={32}
+              color={theme.colors.shape.main}
+            />
+          </ButtonAnimated>
+        </Animated.View>
+      </PanGestureHandler>
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Home;
