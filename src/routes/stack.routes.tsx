006dc6b21c026943b87c2802945276c293b56b68
hitalo <hitalo.2014.hs@gmail.com>
2023-05-12 11:32:30 -0300

    feat: implemented first and second steps on routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,6 +8,9 @@ import ScheduleComplete from "../screens/ScheduleComplete";
 import ScheduleDetails from "../screens/ScheduleDetails";
 import UserCars from "../screens/UserCars";
 import Splash from "../screens/Splash";
+import SignIn from "../screens/SignIn";
+import RegisterFirstStep from "../screens/Register/RegisterFirstStep";
+import RegisterSecondStep from "../screens/Register/RegisterSecondStep";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -20,6 +23,9 @@ function StackRoutes() {
       initialRouteName="Splash"
     >
       <Screen name="Splash" component={Splash} />
+      <Screen name="SignIn" component={SignIn} />
+      <Screen name="RegisterFirstStep" component={RegisterFirstStep} />
+      <Screen name="RegisterSecondStep" component={RegisterSecondStep} />
       <Screen
         name="Home"
         component={Home}
