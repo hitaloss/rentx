4faf7d960569c92d49453e862de2e19c89b5f0c6
hitalo <hitalo.2014.hs@gmail.com>
2023-05-17 11:59:25 -0300

    refactor: changed ScheduleCompleted screen name for new dynamic screen Confirmation

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,13 +4,14 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
 import Schedule from "../screens/Schedule";
 import CarDetails from "../screens/CarDetails";
-import ScheduleComplete from "../screens/ScheduleComplete";
 import ScheduleDetails from "../screens/ScheduleDetails";
 import UserCars from "../screens/UserCars";
 import Splash from "../screens/Splash";
 import SignIn from "../screens/SignIn";
 import RegisterFirstStep from "../screens/Register/RegisterFirstStep";
 import RegisterSecondStep from "../screens/Register/RegisterSecondStep";
+import ScheduleComplete from "../screens/Confirmation";
+import Confirmation from "../screens/Confirmation";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -26,6 +27,7 @@ function StackRoutes() {
       <Screen name="SignIn" component={SignIn} />
       <Screen name="RegisterFirstStep" component={RegisterFirstStep} />
       <Screen name="RegisterSecondStep" component={RegisterSecondStep} />
+      <Screen name="Confirmation" component={Confirmation} />
       <Screen
         name="Home"
         component={Home}
