7e8e5e66cef2e690c9116f73bd72722f1608b1b8
hitalo <hitalo.2014.hs@gmail.com>
2023-05-17 12:15:27 -0300

    feat: created new schema for user register validation

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -4,3 +4,9 @@ export const signInSchema = Yup.object().shape({
     email: Yup.string().required("Email obrigatório").email("Digite um email válido"),
     password: Yup.string().required("Senha é obrigatória")
 })
+
+export const registerSchema = Yup.object().shape({
+    name: Yup.string().required("Nome obrigatório").min(3, "Digite um nome válido"),
+    email: Yup.string().required("Email obrigatório").email("Email válido"),
+    driverLicense: Yup.string().required("CNH é obrigatória").min(11, "CNH inválida")
+})
