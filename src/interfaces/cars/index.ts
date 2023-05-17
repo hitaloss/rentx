12eccd572cd5b2a3af48ff047d64df85dd2fcc6d
hitalo <hitalo.2014.hs@gmail.com>
2023-05-17 11:56:56 -0300

    fix: adapted cars interface for new api usage

--- src/interfaces/cars/index.ts
+++ src/interfaces/cars/index.ts
@@ -3,16 +3,17 @@ export interface ICar {
     brand: string;
     name: string;
     about: string;
-    rent: {
-      period: string;
-      price: number;
-    };
+    period: string;
+    price: number;
     fuel_type: string;
     thumbnail: string;
-    accessories: 
-      {
+    accessories: {
+        id: string;
         type: string;
         name: string;
       }[];
-    photos: string[];
+    photos: {
+      id: string;
+      photo: string;
+    }[];
 }
