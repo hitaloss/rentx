8fccd71073a5969cfcd3c5640783898a94b95b31
hitalo <hitalo.2014.hs@gmail.com>
2023-05-30 04:50:21 -0300

    build: settings for native splash screen image

--- android/app/src/main/java/com/rentx/MainActivity.java
+++ android/app/src/main/java/com/rentx/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import expo.modules.splashscreen.singletons.SplashScreen;
+import expo.modules.splashscreen.SplashScreenImageResizeMode;
 
 
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
   }
 
   /**
