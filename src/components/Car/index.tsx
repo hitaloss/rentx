99dbad36f79073a2fb31aedcf86f57b6819a137d
hitalo <hitalo.2014.hs@gmail.com>
2023-06-12 10:10:21 -0300

    feat: implemented offline functionality on car component

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -18,11 +18,14 @@ import { RectButtonProps } from "react-native-gesture-handler";
 import { Car as ModelCar } from "../../database/models/Car";
 import { getAccessoryIcon } from "../../utils";
 
+import { useNetInfo } from "@react-native-community/netinfo";
+
 interface Props extends RectButtonProps {
   data: ModelCar;
 }
 
 function Car({ data, ...rest }: Props) {
+  const netInfo = useNetInfo();
   const MotorIcon = getAccessoryIcon(data.fuel_type);
 
   return (
@@ -33,7 +36,9 @@ function Car({ data, ...rest }: Props) {
         <About>
           <Rent>
             <Period>{data.period}</Period>
-            <Price>{`R$ ${data.price}`}</Price>
+            <Price>{`R$ ${
+              netInfo.isConnected === true ? data.price : "..."
+            }`}</Price>
           </Rent>
           <Type>
             <MotorIcon />
